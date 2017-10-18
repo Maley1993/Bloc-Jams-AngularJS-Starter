ee3bb7e8d59e4494bcbdf81348b7cee914f1f622
Maley1993 <maley1993@gmail.com>
2017-10-17 17:34:15 -0700

    services part 2 assignment complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,6 +2,10 @@
   function SongPlayer() {
     var SongPlayer = {};
 
+    /**
+    * @desc object thats hold current song
+    * @type {Object}
+    */
     var currentSong = null;
 
     /**
@@ -10,13 +14,23 @@
     */
     var currentBuzzObject = null;
 
+    /**
+    * @function playSong
+    * @desc plays currentBuzzObject and sets value of playing to true
+    * @param {Object} song
+    */
+
+    var playSong = function(song) {
+      currentBuzzObject.play();
+      song.playing = true;
+    };
+
 
     /**
     * @function setSong
     * @desc Stops currently playing song and loads new audio file as currentBuzzObject
     * @param {Object} song
     */
-
     var setSong = function(song) {
       if (currentBuzzObject) {
         currentBuzzObject.stop();
@@ -31,18 +45,28 @@
     currentSong = song;
     };
 
+    /**
+    * @function play
+    * @desc adds play function as property to the SongPlayer object. play function sets the current song file based on the row that is clicked in the album screen, plays the buzz sounds file for the row, and sets the value of 'playing' to true.
+    * @param {Object} song
+    */
     SongPlayer.play = function(song) {
       if (currentSong !== song) {
         setSong(song);
-        currentBuzzObject.play();
-        song.playing = true;
+        playSong(song);
       } else if (currentSong === song) {
          if (currentBuzzObject.isPaused()) {
-             currentBuzzObject.play();
+             playSong(song);
          }
        }
     };
 
+
+    /**
+    * @function pause
+    * @desc adds pause function as property to the SongPlayer object. pause function pauses the buzz sound file and sets value of 'playing' to false.
+    * @param {Object} song
+    */
     SongPlayer.pause = function(song) {
       currentBuzzObject.pause();
       song.playing = false;
